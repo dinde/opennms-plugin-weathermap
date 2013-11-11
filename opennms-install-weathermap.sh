ed1a627e548fc06382b7d11a0dee45748a647584
David Dinde <david@ows.fr>
2013-11-11 22:41:59 +0100

    First Release

---
+++ opennms-install-weathermap.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+## opennms-install-weathermap.sh
+##
+# Author: David "Dinde" OH <david@ows.fr> - http://www.owns.fr
+# Version: 1.0
+# Licence:
+##
+
+## Functions
+# Distrib dectection
+function os_detect(){
+	echo "NOTICE: Detecting if this script is compatible with OS detection."
+	if [ -f /etc/redhat-release ] ; then
+		DISTRO='rpm'
+		echo "NOTICE: rpm based distro detected."
+	elif [ -f /etc/debian_version ] ; then
+		DISTRO='deb'
+		echo "NOTICE: deb based distro detected."
+	else
+		echo "FATAL: I can't detect your distro. Supported distro are: debian/ubuntu, centos/redhat."
+		exit 1
+	fi
+}
+
+function get_pkg(){
+	echo "NOTICE: Downloading the sources ..."
+	git clone https://github.com/dinde/opennms-plugin-weathermap.git opennms-plugin-weathermap
+	if [ "$?" != "0" ] ; then
+		echo "WARNING: git is not installed ! Trying to wget it."
+		wget https://github.com/dinde/opennms-plugin-weathermap/archive/master.zip -O opennms-plugin-weathermap.zip
+		if [ "$?" != "0" ] ; then
+			echo "FATAL: wget failed as well ! Not more method left, exiting ..."
+			exit 1
+		else 
+			echo "NOTICE: opennms-plugin-weathermap.zip downloaded to $(pwd) !"
+			unzip opennms-plugin-weathermap.zip
+			if [ "$?" != "0" ] ; then
+				echo "Can't extract the zip archive. unzip is missing, exiting ..."
+				exit 1
+			fi
+		fi
+	fi
+}
+
+function deploy_deb(){
+	echo "NOTICE: Getting dependencies ..."
+	apt-get install -y librrds-perl libgd-gd2-perl librrd4 
+	echo "NOTICE: Deploying files for deb based distro ..."
+	if [ -d "/usr/share/opennms/jetty-webapps/opennms/" ] ; then
+		cp -ar opennms-plugin-weathermap/webapps/weathermap/ /usr/share/opennms/jetty-webapps/opennms/
+		echo "SUCCESS: webapps deployed !"
+	else
+		echo "FATAL: webapps folder is missing, exiting ..."
+		exit 1
+	fi
+	
+	if [ -d "/etc/opennms/" ] ; then
+		cp -ar opennms-plugin-weathermap/configs/etc/opennms/weathermap/ /etc/opennms/
+		cp -ar opennms-plugin-weathermap/configs/etc/opennms/weathermap.enable /etc/opennms/
+		echo "SUCCESS: configuration files deployed !"
+	else
+		echo "FATAL: /etc/opennms/ folder is missing, exiting ..."
+		exit 1
+	fi
+
+	if [ -d "/usr/share/opennms/bin/" ] ; then
+		cp -ar opennms-plugin-weathermap/patchs/weathermap4-rrd-1.2RC3/patched_files/weathermap4rrd /usr/share/opennms/bin/
+		chmod +x /usr/share/opennms/bin/weathermap4rrd
+		echo "SUCCESS: weathermap4rrd deployed !"
+	else
+		echo "FATAL: Opennms bin folder is missing, exiting ..."
+		exit 1
+	fi
+	
+	if [ -f "/usr/share/opennms/jetty-webapps/opennms/WEB-INF/dispatcher-servlet.xml" ] ; then
+		patch /usr/share/opennms/jetty-webapps/opennms/WEB-INF/dispatcher-servlet.xml < opennms-plugin-weathermap/patchs/opennms-webapps/dispatcher-servlet.xml.patch
+		echo "SUCCESS: dispatcher-servlet.xml patched !"
+	else
+		echo "FATAL: dispatcher-servlet.xml is missing, exiting ..."
+		exit 1
+	fi
+
+	if [ -d "/etc/cron.d/" ] ; then
+		if [ ! -f "/etc/cron.d/opennms_weathermap" ] ; then
+			echo -e "#!/bin/bash \n */5 * * * for i in \$(ls /etc/opennms/weathermap/*.conf) \; do /usr/share/opennms/bin/weathermap4rrd -c \$i \; done" > /etc/cron.d/opennms_weathermap
+			chmod +x /etc/cron.d/opennms_weathermap
+			echo "SUCCESS: crontab deployed to /etc/cron.d/opennms_weathermap !"
+		else
+			echo "WARNING: files already exists."
+		fi
+	else
+		echo "FATAL: cron is not installed, exiting ..."
+	fi
+}
+
+function deploy_rpm(){
+	echo "NOTICE: Getting dependencies ..."
+	yum install -y zip patch perl-GD rrdtool-perl
+	echo "NOTICE: Deploying files for rpm based distro ..."
+    if [ -d "/opt/opennms/jetty-webapps/opennms/" ] ; then
+        cp -ar opennms-plugin-weathermap/webapps/weathermap/ /opt/opennms/jetty-webapps/opennms/
+        echo "SUCCESS: webapps deployed !"
+    else
+        echo "FATAL: webapps folder is missing, exiting ..."
+        exit 1
+    fi
+
+    if [ -d "/opt/opennms/etc/" ] ; then
+        cp -ar opennms-plugin-weathermap/configs/etc/opennms/weathermap/ /opt/opennms/etc/
+        cp -ar opennms-plugin-weathermap/configs/etc/opennms/weathermap.enable /opt/opennms/etc/
+        echo "SUCCESS: configuration files deployed !"
+    else
+        echo "FATAL: /etc/opennms/ folder is missing, exiting ..."
+        exit 1
+    fi
+
+    if [ -d "/opt/opennms/bin/" ] ; then
+        cp -ar opennms-plugin-weathermap/patchs/weathermap4-rrd-1.2RC3/patched_files/weathermap4rrd /opt/opennms/bin/
+        chmod +x /opt/opennms/bin/weathermap4rrd
+		echo "SUCCESS: weathermap4rrd deployed !"
+    else
+        echo "FATAL: Opennms bin folder is missing, exiting ..."
+        exit 1
+    fi
+
+    if [ -f "/opt/opennms/jetty-webapps/opennms/WEB-INF/dispatcher-servlet.xml" ] ; then
+        patch /opt/opennms/jetty-webapps/opennms/WEB-INF/dispatcher-servlet.xml < opennms-plugin-weathermap/patchs/opennms-webapps/dispatcher-servlet.xml.patch
+        echo "SUCCESS: dispatcher-servlet.xml patched !"
+    else
+        echo "FATAL: dispatcher-servlet.xml is missing, exiting ..."
+        exit 1
+    fi
+
+    if [ -d "/etc/cron.d/" ] ; then
+        if [ ! -f "/etc/cron.d/opennms_weathermap" ] ; then
+            echo -e "#!/bin/bash \n */5 * * * for i in \$(ls /etc/opennms/weathermap/*.conf) \; do /opt/opennms/bin/weathermap4rrd -c \$i \; done" > /etc/cron.d/opennms_weathermap
+            chmod +x /etc/cron.d/opennms_weathermap
+            echo "SUCCESS: crontab deployed to /etc/cron.d/opennms_weathermap !"
+        else
+            echo "WARNING: files already exists."
+        fi
+    else
+        echo "FATAL: cron is not installed, exiting ..."
+    fi
+
+}
+
+## Start
+get_pkg
+
+os_detect
+
+if [ "$DISTRO" = 'deb' ] ; then
+	deploy_deb
+elif [ "$DISTRO" = 'rpm' ] ; then
+	deploy_rpm
+else
+	echo "FATAL: How did you arrive until here ... Distribution not detected, exiting ...."
+fi
+
+echo "ENDED: Don't forget to check out all required dependecies."
+echo "INFO: It's now time to configure your default map thru default.conf file !!!"
+echo "HELP ?: For more informations, visit: https://github.com/dinde/opennms-plugin-weathermap/ & http://www.owns.fr !"
+
+exit 0
---
+++ webapps/weathermap/index.jsp
@@ -0,0 +1,90 @@
+<%--
+/*******************************************************************************
+ * opennms-plugin-weathermap is an integration of a patched version of weathermap4rrd on Opennms.
+ * It adds a menu link on Opennms UI under Maps called WeatherMap.
+ * It supports multiple maps thru a drop-box and reload itself every 60s.
+ * All maps must be on weathermap/img/maps folder.
+ * The weathermap itself is a generated png file by crontab entry.
+ * Icons are located in weathermap/img/icons folder.
+ * Author: David "Dinde" OH <david@ows.fr> - http://www.owns.fr
+ * Date: 01/11/2013 -> Patch-1 by: David Dinde <david@ows.fr> to feature
+ * Version: 1.0
+ * Licence:
+*******************************************************************************/
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java"%>
+
+<% 
+   String breadcrumb1 = "Weather Map";
+
+   //avoid cache
+   response.setHeader("Cache-Control","no-store");
+   response.setHeader("Pragma","no-cache");
+   response.setHeader("Expires","0"); 
+%>
+		<jsp:include page="/includes/header.jsp" flush="false">
+		  <jsp:param name="title" value="Display Weather Map" />
+		  <jsp:param name="breadcrumb" value="<%=breadcrumb1%>" />
+		</jsp:include>
+
+<%@ page import="java.io.*" %>
+
+<p>
+
+<body>
+<% 
+	// List all png files on img dir
+        String file = application.getRealPath("/weathermap/img/maps"); 
+        File f = new File(file);
+        String [] fileNames = f.list();
+        File [] fileObjects= f.listFiles(new FilenameFilter() {
+		@Override
+		public boolean accept(File dir, String name) {
+			return name.endsWith(".png");
+		}
+	});
+%>
+	<%-- Form --%>
+	<br>
+	<form name="weatherMap" action="javaScript:setImage();">
+        	<select name="default" onChange="setImage(this.value);">
+<%
+        for (int i = 0; i < fileObjects.length; i++) {
+                if(!fileObjects[i].isDirectory()){
+		String baseName = fileNames[i].replace(".png", "");
+%>
+		<option value="<%= fileNames[i] %>"> <%= baseName %> </option>
+<%
+                }
+        }
+%>
+        	</select>
+		<noscript><input type="submit" value="Submit" action="index.jsp"></noscript>
+	</form>
+
+<br><img id="pngmap" src="/opennms/weathermap/img/maps/default.png" /><br><br>
+
+<script>
+image = "default.png"
+function setImage(newImage) {
+        image = newImage
+        getData();
+}
+
+function getData() {
+	document.getElementById("pngmap").src = "/opennms/weathermap/img/maps/" + image + "?" + new Date().getTime();
+	setTimeout(getData, 60000);
+}
+getData();
+
+</script>
+
+</body>
+
+</p>
+
+    <jsp:include page="/includes/footer.jsp" flush="false" >
+      <jsp:param name="location" value="weathermap" />
+    </jsp:include>
+    
